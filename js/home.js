7bbad5d3c67e7e8cfa89bf9614d444c1c974cc08
Angelica Siena Abaya <siena.abaya@aestrea.com>
2015-01-22 19:55:28 +0800

    add animation to side bar when scrolled in to rooms and scrolled out of rooms
    Signed-off-by: Angelica Siena Abaya <siena.abaya@aestrea.com>

--- js/home.js
+++ js/home.js
@@ -1,3 +1,28 @@
 $(function(){
-
+    showSidebar();
+    $(document).scroll(function () {
+        showSidebar();
+    });
 });
+
+function showSidebar(){
+    if($(document).scrollTop() >= $('#rooms').offset().top){
+        if(!$('#sidebar > img').hasClass('showIcon')){
+            $('#sidebar > img').each(function(i){
+                $(this).delay( i * 100).animate({right:'30px'});
+                $(this).addClass('showIcon');
+                if( $(this).hasClass('hideIcon'))
+                    $(this).removeClass('hideIcon');
+            });
+        }
+    }else{
+        if(!$('#sidebar > img').hasClass('hideIcon')){
+            $('#sidebar > img').each(function(i){
+                $(this).delay( i * 100).animate({right:'-150px'});
+                $(this).addClass('hideIcon');
+                if( $(this).hasClass('showIcon'))
+                    $(this).removeClass('showIcon');
+            });
+        }
+    }
+}
