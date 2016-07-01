54d7fe9e1b8bb6f2dc0a6a55a84469aa52722cf6
Chuckchang23 <chuck@mysterymanila.com>
2016-07-01 11:47:21 +0800

    asd

--- js/front.js
+++ js/front.js
@@ -172,14 +172,7 @@ mm.runInstagramFeed = function () {
                 sortBy: 'most-recent',
                 template: '<a href="{{link}}" target="_blank" class="col-md-2 col-sm-2 col-xs-6"><img src="{{image}}" /></a>',
                 resolution: 'low_resolution',
-                success: function(feed){
-                    var data = feed.data.reverse();
-                    $('.placeholder').each(function(index, placeholder){
-                        var model = data.pop();
-                        $(placeholder).html('<a href="'+ model.link +'" target="_blank">'
-                                + '<img src="'+ model.images.low_resolution.url +'" /></a>').hide().fadeIn();
-                    });
-                }
+                
             }).run();
         }
     });
