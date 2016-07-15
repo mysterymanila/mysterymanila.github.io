f0a578bb4461173348076edd7aba9bc0322f78f7
rensramos <rensramos17@gmail.com>
2016-07-15 13:05:19 +0800

    Update front.js

--- js/front.js
+++ js/front.js
@@ -166,8 +166,9 @@ mm.runInstagramFeed = function () {
         if (Instafeed) {
             new Instafeed({
                 get: 'tagged',
-                tagName: 'MYSTERYMANILA',
+                tagName: 'mysterymanila',
                 clientId: '0cd51b94022a49f0be1e4b114f24a9ea',
+                accessToken: "1162756535.0cd51b9.7b8b57dd36494ff08f9e4e5c976742b7",
                 limit: 18,
                 sortBy: 'most-recent',
                 template: '<a href="{{link}}" target="_blank" class="col-md-2 col-sm-2 col-xs-6"><img src="{{image}}" /></a>',
