897b8483fcec2ffee93428581e3418132b619c32
chuck@mysterymanila.com <chuck@mysterymanila.com>
2024-07-25 12:32:06 +0800

    carina rainstorm advisory

--- js/front.js
+++ js/front.js
@@ -323,9 +323,9 @@ $(function(){
     // console.log(new Date('2019-09-19'));
     // if(new Date() < new Date('2019-12-24')) {
         
-        $('#mpass').modal('show');
+        $('#overlay').modal('show');
          setTimeout(function() {
-             $('#mpass').modal('hide');
+             $('#overlay').modal('hide');
          }, 10000);
     // }
 });
